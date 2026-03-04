cbfeeb94dc0480a6898f210e7ea545c145a5be85
Zhou-Quan Wan <zwan1@ccqlin035.flatironinstitute.org>
2026-03-04 17:25:50 -0500

    change the ansatz; modify the randomization

--- src/dressed_rbm.py
+++ src/dressed_rbm.py
@@ -13,21 +13,16 @@ class DressedRBM(nn.Module):
 
         ns = x.shape[-1]
         sum_x = jnp.sum(x, axis=-1)
-
-        amp_spin = self.param(
-            "amp_spin",
-            lambda key, shape: jnp.array([0.0]*(ns-1), dtype = jnp.complex128),
-            (ns-1,)
-        )
-
-        # global shared log-amplitude for all sectors n_up>=1
-        eps = self.param(
-            "eps",
-            lambda key: jnp.asarray(self.amp_init, dtype=jnp.float64).astype(jnp.complex128)
-        )
-        a_s =  jnp.concatenate((jnp.array([1.0, eps], dtype = jnp.complex128), 
-                                self.amp_init + amp_spin))
         index = (ns + sum_x)//2
-        logF =  jnp.log(a_s[index]) 
+        baseline = jnp.array([1.0]+ [self.amp_init]*(ns) , dtype = jnp.complex128)
 
+        thetas = self.param(
+            "thetas",
+            lambda key, shape: jnp.array([0.0]*(ns), dtype = jnp.complex128),
+            (ns,)
+        )
+
+        eps = jnp.concatenate([jnp.array([0.0], dtype = jnp.complex128), thetas])
+        
+        logF =  jnp.log(baseline[index]+ eps[index] + 1 * jnp.where(index==2, eps[1], 0.0)) 
         return logpsi_rbm + logF
